d9bcedd303758258af3c65579a72333bc2e02354
FahimSakib <fsakib991@gmail.com>
2022-09-02 22:21:58 +0600

    frontend navbar

--- resources/views/frontend/include/header.blade.php
+++ resources/views/frontend/include/header.blade.php
@@ -67,57 +67,28 @@
                         <div class="collapse navbar-collapse sub-menu-bar" id="navbarSupportedContent">
                             <ul class="navbar-nav mr-auto">
                                 <li class="nav-item">
-                                    <a class="active" href="index-2.html">Home</a>
-                                    <ul class="sub-menu">
-                                        <li><a class="active" href="index-2.html">Home 01</a></li>
-                                        <li><a href="index-3.html">Home 02</a></li>
-                                        <li><a href="index-4.html">Home 03</a></li>
-                                    </ul>
+                                    <a class="{{ (request()->is('/')) ? 'active' : '' }}" href="{{ route('index') }}">Home</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a href="about.html">About us</a>
+                                    <a class="{{ (request()->is('about')) ? 'active' : '' }}" href="{{ route('about') }}">About us</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a href="courses.html">Courses</a>
-                                    <ul class="sub-menu">
-                                        <li><a href="courses.html">Courses</a></li>
-                                        <li><a href="courses-singel.html">Course Singel</a></li>
-                                    </ul>
+                                    <a class="{{ (request()->is('courses')) ? 'active' : '' }}" href="{{ route('courses') }}">Courses</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a href="events.html">Events</a>
-                                    <ul class="sub-menu">
-                                        <li><a href="events.html">Events</a></li>
-                                        <li><a href="events-singel.html">Event Singel</a></li>
-                                    </ul>
+                                    <a class="{{ (request()->is('events')) ? 'active' : '' }}" href="{{ route('events') }}">Events</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a href="teachers.html">Our teachers</a>
-                                    <ul class="sub-menu">
-                                        <li><a href="teachers.html">teachers</a></li>
-                                        <li><a href="teachers-singel.html">teacher Singel</a></li>
-                                    </ul>
+                                    <a class="{{ (request()->is('teachers')) ? 'active' : '' }}" href="{{ route('teachers') }}">Our Teachers</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a href="blog.html">Blog</a>
-                                    <ul class="sub-menu">
-                                        <li><a href="blog.html">Blog</a></li>
-                                        <li><a href="blog-singel.html">Blog Singel</a></li>
-                                    </ul>
+                                    <a class="{{ (request()->is('blog')) ? 'active' : '' }}" href="{{ route('blog') }}">Blog</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a href="shop.html">Shop</a>
-                                    <ul class="sub-menu">
-                                        <li><a href="shop.html">Shop</a></li>
-                                        <li><a href="shop-singel.html">Shop Singel</a></li>
-                                    </ul>
+                                    <a class="{{ (request()->is('shop')) ? 'active' : '' }}" href="{{ route('shop') }}">Shop</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a href="contact.html">Contact</a>
-                                    <ul class="sub-menu">
-                                        <li><a href="contact.html">Contact Us</a></li>
-                                        <li><a href="contact-2.html">Contact Us 2</a></li>
-                                    </ul>
+                                    <a class="{{ (request()->is('contact')) ? 'active' : '' }}" href="{{ route('contact') }}">Contact</a>
                                 </li>
                             </ul>
                         </div>
